c0e59cf532241a324a71357ffb5659a536ee6168
Alex <mak@MacBook-Air.local>
2024-07-17 19:12:45 +0300

    removed the storyboard

---
+++ CharactersApp-api/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CharactersApp-api
+//
+//  Created by Alex  on 17.07.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		
+		let window = UIWindow()
+		window.rootViewController = ViewController()
+		window.makeKeyAndVisible()
+		self.window = window
+		return true
+	}
+
+
+}
+
---
+++ CharactersApp-api/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CharactersApp-api
+//
+//  Created by Alex  on 17.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .red
+	}
+
+
+}
+
